20ecbf65869cc929e4f0d5aad491aa2a37445ae2
twitf <twitf@twitfdeHackintosh.local>
2020-01-22 21:26:18 +0800

    add error level

--- publish/cors.php
+++ publish/cors.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     'allow_credentials' => false,
 
--- src/Middleware/CorsMiddleware.php
+++ src/Middleware/CorsMiddleware.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Twitf\Cors\Middleware;
 
 use Twitf\Cors\Cors;
